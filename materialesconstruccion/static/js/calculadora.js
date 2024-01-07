bb6c6a881d01cb0baee3a028292d210cdba5189e
kelvin <kelvindazaflores@gmail.com>
2024-01-06 18:05:40 -0600

    modifico los estilos

--- materialesconstruccion/static/js/calculadora.js
+++ materialesconstruccion/static/js/calculadora.js
@@ -1,4 +1,16 @@
 let i=1
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  } 
+
 function calcularSubtotal(){
     let costounit=document.querySelectorAll(".costounit");
     let cantidad=document.querySelectorAll(".cantidad");
